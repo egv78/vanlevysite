e59ce32d682849382f4c2ee7baf4be627ea08eab
Eric Van Inwegen <git@ericvaninwegen.net>
2018-12-12 08:54:02 -0500

    Static files? - I hope (whitenoise on prod)

--- vanlevysite/settings/prod.py
+++ vanlevysite/settings/prod.py
@@ -12,3 +12,20 @@ try:
 except:
     pass
 
+
+# these handle static files
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# end static files
+
