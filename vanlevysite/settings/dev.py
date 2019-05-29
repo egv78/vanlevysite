a216c4f68d79649b3a1e95b24b46c0d21f5058ca
Eric Van Inwegen <git@ericvaninwegen.net>
2019-05-29 11:52:09 -0400

    Emails now working Updated dev, prod, email, and request sent page

--- vanlevysite/settings/dev.py
+++ vanlevysite/settings/dev.py
@@ -10,10 +10,3 @@ except:
 
 DEBUG = True
 
-EMAIL_HOST = auth_dict['smtp_host']
-EMAIL_PORT = 587
-EMAIL_HOST_USER = auth_dict['smtp_username']
-EMAIL_HOST_PASSWORD = auth_dict['smtp_password']
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
-DEFAULT_FROM_EMAIL = 'webmaster@vanlevy.com'
--- vanlevysite/settings/prod.py
+++ vanlevysite/settings/prod.py
@@ -69,3 +69,12 @@ DATABASES = {
 # CONN_MAX_AGE = 1800
 # end database
 
+# Email Settings
+EMAIL_HOST = auth_dict['smtp_host']
+EMAIL_PORT = 587
+EMAIL_HOST_USER = auth_dict['smtp_username']
+EMAIL_HOST_PASSWORD = auth_dict['smtp_password']
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+DEFAULT_FROM_EMAIL = 'webmaster@vanlevy.com'
+
